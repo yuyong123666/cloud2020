f06b8f310b14a85a3120f463d6781d038b858311
YuYong <2682175453@qq.com>
2022-09-20 19:56:42 +0800

    seventh commit

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/spring/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/spring/alibaba/controller/FlowLimitController.java
@@ -1,5 +1,7 @@
 package com.atguigu.spring.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,8 +43,15 @@ public class FlowLimitController {
         return "------testE";
     }
 
-    public String testHotKey(@RequestParam()) {
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "---------testHotKey";
+    }
 
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "----------deal_testHotKey";
     }
 
 }
